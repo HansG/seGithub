e033a40d03a2faead3ef2ed6176919d97bf162d6
HansG <hans.gail@akdb.de>
2022-07-04 18:15:00 +0200

    @AKDB  CatsTry ReaderTry scala-with-cats S.115

--- modules/tests/src/test/scala/exa/Cats1.sc
+++ modules/tests/src/test/scala/exa/Cats1.sc
@@ -16,6 +16,7 @@ val job = IO.monotonic <* IO.sleep(10.seconds)
 def slowly[A](body: => A) =
   try body finally Thread.sleep(100)
 
+/*
 def factorial(n: Int): WriterT[Id, String, Int] = slowly(
   if(n == 0)  1.writer("fact 0 = 1")
    else
@@ -30,33 +31,38 @@ val re = Await.result(Future.sequence(Vector(
 val tes = re.map( f => f.run)
 
 tes.map(log_f =>  println(s"Fact: ${log_f._2}\nHist:\n"+log_f._1) )
-
+*/
 
 import cats.data.Writer
 import cats.instances.vector._
 import cats.syntax.applicative._ // for pure
-type Logged[A] = Writer[Vector[String], A]
+import cats.syntax.writer._ // for tell
+import cats.instances.vector._ // for Monoid
+
+
+type Logged[A] = WriterT[Id, Vector[String], A]
 42.pure[Logged]
 
-import cats.syntax.writer._ // for tell
 Vector("Message").tell
 
-import cats.instances.vector._ // for Monoid
 41.pure[Logged].flatMap( f =>  (f + 1).pure[Logged])
 
-def factorial1(n: Int): Logged[Int] =
+
+def factorial1(n: BigInt) : Logged[BigInt] =
   for  {
     ans <- if(n == 0) {
-              1.pure[Logged]
-            } else {
-            slowly(factorial(n - 1).map(_ * n))
-            }
-  _ <- Vector(s"fact $n $ans").tell
+      BigInt(1).pure[Logged]
+    } else {
+      slowly(factorial1(  n - 1  ).map(_ *  n ))
+    }
+    _ <- Vector(s"fact $n $ans").tell
   } yield ans
 
 val (log, res) = factorial1(5).run
 
-Await.result(Future.sequence(Vector(
-  Future(factorial1(5)),
-  Future(factorial1(5))
-)).map(_.map(_.written)), 5.seconds)
+val logs = Await.result(Future.sequence(Vector(
+  Future(factorial1(50)),
+  Future(factorial1(40))
+)).map(_.map(_.written.mkString(", ")  )), 5.seconds)
+
+println(logs.mkString("\nNext:\n"))
--- modules/tests/src/test/scala/exa/CatsTry.scala
+++ modules/tests/src/test/scala/exa/CatsTry.scala
@@ -1,28 +1,118 @@
 package exa
 
-import cats.Eval
+import cats.data.Reader
+import natchez.Tags.db
 
-object CatsTry {
-
-
-  def slowly[A](body: => A) =
-    try body finally Thread.sleep(100)
-
-  def factorial(n: Int): Int = {
-    val ans = slowly(if(n == 0) 1 else n * factorial(n - 1))
-    println(s"fact $n $ans")
-    ans
-  }
+import scala.meta.internal.Scaladoc.TagType.See
 
 
-  def factorial(n: BigInt): Eval[BigInt] =
-    if(n == 1) {
-      Eval.now(n)
-    } else {
-      factorial(n - 1).map(_ * n)
+object CatsTry extends App{
+  import cats.{Eval, Id}
+  import cats.syntax.applicative._ // for pure
+  import cats.instances.vector._ // for Monoid
+  import scala.concurrent.ExecutionContext.Implicits._
+  import scala.concurrent.{Await, Future}
+  import scala.concurrent.duration._
+
+  object Writer  {
+    import cats.data.Writer
+    import cats.data.WriterT
+    import cats.syntax.writer._ // for tell
+
+
+
+    def slowly[A](body: => A) =
+      try body finally Thread.sleep(100)
+
+    def factorial(n: Int): Int = {
+      val ans = slowly(if(n == 0) 1 else n * factorial(n - 1))
+      println(s"fact $n $ans")
+      ans
     }
 
 
+    def factorial(n: BigInt): Eval[BigInt] =
+      if(n == 1) {
+        Eval.now(n)
+      } else {
+        factorial(n - 1).map(_ * n)
+      }
+
+    type Logged[A] = WriterT[Id, Vector[String], A]
+
+    val writer1: WriterT[Id, Vector[String], Int] = for {
+      a <- 10.pure[Logged]
+      _ <- Vector("a", "b", "c").tell
+      b <- 32.writer(Vector("x", "y", "z"))
+    } yield a + b
+
+    def factorial1(n: BigInt) : Logged[BigInt] =
+      for  {
+        ans <- if(n == 0) {
+          BigInt(1).pure[Logged]
+        } else {
+          slowly(factorial1(  n - 1  ).map(_ *  n ))
+        }
+        _ <- Vector(s"fact $n $ans").tell
+      } yield ans
+
+    val logs = Await.result(Future.sequence(Vector(
+      Future(factorial1(50)),
+      Future(factorial1(40))
+    )).map(_.map(_.mapWritten( _.mkString(", "))  )), 5.seconds)
+    // )).map(_.map(_.written.mkString(", ")  )), 5.seconds)
+
+    println(logs.mkString("\nNext:\n"))
 
 
-}
+  }
+
+  object ReaderTry {
+
+    final case class Db( usernames: Map[Int, String],
+                         passwords: Map[String, String] )
+
+    type DbReader[A] = Reader[Db, A]
+
+    def findUsername(userId: Int): DbReader[Option[String]] = Reader(db => db.usernames.get(userId))
+
+
+    def checkPassword( username: String,
+                       password: String): DbReader[Boolean] = Reader( db => db.passwords.get(username).fold(false)(_ == password) )
+
+    def checkLogin( userId: Int,
+                    password: String): DbReader[Boolean] = findUsername(userId).flatMap { userNameOpt =>
+      userNameOpt.fold(Reader((_: Db) => false))((userName: String) => checkPassword(userName, password))
+    }
+
+
+    def checkLogin1( userId: Int,
+                    password: String): DbReader[Boolean] =
+      for {
+        username <- findUsername(userId)
+        passwordOk <- username.map { username =>
+          checkPassword(username, password)
+        }.getOrElse {
+          false.pure[DbReader]
+        }
+      } yield passwordOk
+
+
+    val users = Map(
+      1 -> "dade",
+      2 -> "kate",
+      3 -> "margo"
+    )
+    val passwords = Map(
+      "dade" -> "zerocool",
+      "kate" -> "acidburn",
+      "margo" -> "secret"
+    )
+    val db = Db(users, passwords)
+
+    println(checkLogin(1, "zerocool").run(db))
+    println(checkLogin(4, "davinci").run(db))
+
+
+  }
+ }
