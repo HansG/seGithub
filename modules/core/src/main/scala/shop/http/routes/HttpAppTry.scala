052fdc7cbe5294284857c58c6400044d0272cd50
HG <h.gail@web.de>
2022-09-27 12:28:05 +0200

    @home  HttpAppTry.scala

--- modules/core/src/main/scala/shop/http/routes/HttpAppTry.scala
+++ modules/core/src/main/scala/shop/http/routes/HttpAppTry.scala
@@ -2,9 +2,9 @@ package shop.http.routes
 
 import cats.effect._
 import cats.syntax.all._
-import com.comcast.ip4s.{Host, IpLiteralSyntax, Port}
-import derevo.cats.{eqv, show}
-import derevo.circe.magnolia.{decoder, encoder}
+import com.comcast.ip4s.{ Host, IpLiteralSyntax, Port }
+import derevo.cats.{ eqv, show }
+import derevo.circe.magnolia.{ decoder, encoder }
 import derevo.derive
 import eu.timepit.refined.types.string.NonEmptyString
 import io.circe.syntax.EncoderOps
@@ -13,39 +13,32 @@ import monocle.Iso
 import org.http4s.Method._
 import org.http4s._
 import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
-import org.http4s.circe.{JsonDecoder, toMessageSyntax}
+import org.http4s.circe.{ JsonDecoder, toMessageSyntax }
 import org.http4s.client.Client
 import org.http4s.client.dsl.Http4sClientDsl
 import org.http4s.dsl.Http4sDsl
 import org.http4s.implicits.http4sKleisliResponseSyntaxOptionT
-import org.http4s.server.middleware.{RequestLogger, ResponseLogger}
+import org.http4s.server.middleware.{ RequestLogger, ResponseLogger }
 import org.scalacheck.Gen
 import org.scalacheck.rng.Seed
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 import java.util.UUID
-import scala.concurrent.duration.{DurationInt, FiniteDuration}
-import cats.{Monad, MonadThrow}
+import scala.concurrent.duration.{ DurationInt, FiniteDuration }
+import cats.{ Monad, MonadThrow }
 import cats.data.Kleisli
 import cats.effect.kernel.Resource
 import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.server.{Router, Server}
+import org.http4s.server.{ Router, Server }
 import upickle.default
 import eu.timepit.refined.auto._
 import io.circe.Decoder
 import io.circe.generic.codec.DerivedAsObjectCodec.deriveCodec
 import shop.domain.order.PaymentError
-import shop.http.routes.HttAppTry.{CUri, ServiceAtRoute}
-
-
-
-
-
 
 object HttAppTry extends IOApp.Simple {
-
 
   object Data {
     @derive(decoder, encoder, eqv, show) //, uuid
@@ -79,7 +72,7 @@ object HttAppTry extends IOApp.Simple {
       }
     }
 
-    def make[F[_]: Sync, T : IsUUID] = Sync[F].delay(UUID.randomUUID()).map(IsUUID[T]._UUID.get(_))
+    def make[F[_]: Sync, T: IsUUID] = Sync[F].delay(UUID.randomUUID()).map(IsUUID[T]._UUID.get(_))
   }
 
   import Data._
@@ -109,7 +102,7 @@ object HttAppTry extends IOApp.Simple {
         n <- prodNameGen
       } yield Prod(i, n)
 
-    val bg                     = Gen.listOf(prodGen)
+    val bg = Gen.listOf(prodGen)
 
     val nonEmptyPList: Gen[List[Prod]] =
       Gen
@@ -127,8 +120,8 @@ object HttAppTry extends IOApp.Simple {
 
   import shop.ext.http4s.refined._
 
-  class ServiceAtRoute[F[_]: Monad : JsonDecoder : MonadThrow](service: Service[F]) extends Http4sDsl[F] {
-    private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
+  class ServiceAtHttpApp(service: Service[IO]) extends Http4sDsl[IO] {
+    val httpRoutes: HttpRoutes[IO] = HttpRoutes.of[IO] {
       case GET -> Root => Ok(service.findAll)
       case req @ POST -> Root =>
         req.decodeR[ProdName] { pn =>
@@ -138,52 +131,52 @@ object HttAppTry extends IOApp.Simple {
           }
         }
     }
-
-    val routes: HttpRoutes[F] = Router(
-      ServiceAtRoute.prefixPath -> httpRoutes
-    )
-  }
-  object ServiceAtRoute {
-    val prefixPath = "/prods"
   }
 
-  val serviceAtR: HttpRoutes[IO] = new ServiceAtRoute[IO](AMockService.genMockService).routes
-  val httpApp: HttpApp[IO] = Router(version.v1 -> serviceAtR).orNotFound
-
-  def addLoggers(http: HttpApp[IO]): HttpApp[IO] = {
-    val httpReq = RequestLogger.httpApp(true, true)(http)
-    ResponseLogger.httpApp(true, true)(httpReq)
-  }
-  val httpAppl = addLoggers(httpApp)
-
-
-
-  @newtype case class CUri(value: NonEmptyString)
-
-  case class ServerConfig(host: Host, port: Port) {
-    val uri = CUri(NonEmptyString.from(s"http://$host:$port").getOrElse("http://localhost:8080"))
+  trait UriConfigI {
+    val host: Host = host"127.0.0.1"
+    val port: Port = port"8080"
+    val vers       = version.v1
+    val pathPrefix = "/prods"
+    val uri : IO[Uri]
   }
 
-  val serverConfig = ServerConfig(
-    host = host"127.0.0.1",
-    port = port"8080"
-  )
+  object UriConfig extends UriConfigI{
 
-  def httpAppAtServer[F[_]: Async](cfg: ServerConfig, httpApp: HttpApp[F]) =
+    def apply(hostp: Host = host, portp: Port = port): IO[Uri] = {
+      val uris = s"http://$host:$port"
+      Uri.fromString(uris + vers + pathPrefix).liftTo[IO]
+      //alternativ:
+      Uri.fromString(uris + vers).liftTo[IO].map(_ / "prods")
+    }
+
+    override val uri: IO[Uri] = apply()
+  }
+
+  def serviceAtHttpApp(service: AMockService): HttpApp[IO] = {
+    def addLoggers[F[_]: Async](http: HttpApp[F]): HttpApp[F] = {
+      val httpReq = RequestLogger.httpApp(true, true)(http)
+      ResponseLogger.httpApp(true, true)(httpReq)
+    }
+
+    val satr                  = new ServiceAtHttpApp(service)
+    val satrr: HttpRoutes[IO] = Router(UriConfig.pathPrefix -> satr.httpRoutes)
+    val app                   = Router(UriConfig.vers -> satrr).orNotFound
+    addLoggers(app)
+  }
+
+  def httpAppAtServer[F[_]: Async](httpApp: HttpApp[F]) =
     EmberServerBuilder
       .default[F]
-      .withHost(cfg.host)
-      .withPort(cfg.port)
+      .withHost(UriConfig.host)
+      .withPort(UriConfig.port)
       .withHttpApp(httpApp)
       .build
 
-
   def start = {
-    val server  = httpAppAtServer[IO](serverConfig, httpAppl)
+    val server = httpAppAtServer[IO](serviceAtHttpApp(AMockService.genMockService))
     server.useForever
   }
-
-
 
   override def run: IO[Unit] = start
   // override def run: IO[Unit] = runFst
@@ -198,71 +191,61 @@ object HttAppTry extends IOApp.Simple {
 
 //Client ------------------------------------------------------
 
-
 object HttAppClientTry extends IOApp.Simple {
-  import HttAppTry.Data._
+  import HttAppTry.{UriConfigI, UriConfig, Data }
+  import Data._
 
-
-  trait ClientToApp {
+  trait ClientToAppI {
     def getProds: IO[List[Prod]]
     def postProds(prodP: ProdName): IO[Prod]
   }
 
-  object ClientToApp {
-    def apply(client: Client[IO], uri: CUri): ClientToApp =
-      new ClientToApp with Http4sClientDsl[IO] {
-        def getProds: IO[List[Prod]] =
-          Uri.fromString(uri.value + version.v1 + ServiceAtRoute.prefixPath).liftTo[IO].flatMap { uri =>
-            client.run(GET(uri)).use { resp =>
-              resp.status match {
-                case Status.Ok | Status.Conflict =>
-                  resp.asJsonDecode[List[Prod]]
-                case ex =>
-                  PaymentError(
-                    Option(ex.reason).getOrElse("unknown")
-                  ).raiseError[IO, List[Prod]]
-              }
-            }
-          }
+  case class ClientToServer(client: Client[IO], uriConfig: UriConfigI) extends ClientToAppI with Http4sClientDsl[IO] {
 
-        def postProds(pn: ProdName): IO[Prod] =
-          Uri.fromString(uri.value + version.v1).liftTo[IO].flatMap { uri =>
-            client.run(POST(pn, uri / "prods")).use { resp =>
-              resp.status match {
-                case Status.Created | Status.Conflict =>
-                  resp.asJsonDecode[Prod]
-                case st =>
-                  PaymentError(
-                    Option(st.reason).getOrElse("unknown")
-                  ).raiseError[IO, Prod]
-              }
-            }
+    def getProds: IO[List[Prod]] =
+      uriConfig.uri.flatMap { uri =>
+        client.run(GET(uri)).use { resp =>
+          resp.status match {
+            case Status.Ok | Status.Conflict =>
+              resp.asJsonDecode[List[Prod]]
+            case ex =>
+              PaymentError(
+                Option(ex.reason).getOrElse("unknown")
+              ).raiseError[IO, List[Prod]]
           }
+        }
+      }
+
+    def postProds(pn: ProdName): IO[Prod] =
+      uriConfig.uri flatMap { uri =>
+        client.run(POST(pn, uri)).use { resp =>
+          resp.status match {
+            case Status.Created | Status.Conflict =>
+              resp.asJsonDecode[Prod]
+            case st =>
+              PaymentError(
+                Option(st.reason).getOrElse("unknown")
+              ).raiseError[IO, Prod]
+          }
+        }
       }
   }
 
-  case class ClientConfig(timeout: FiniteDuration, idleTimeInPool: FiniteDuration)
-  val clientC = ClientConfig(
-    timeout = 60.seconds,
-    idleTimeInPool = 30.seconds
-  )
 
-  def newClient[F[_]: Async](c: ClientConfig): Resource[F, Client[F]] =
+  def newClient[F[_]: Async](timeout: FiniteDuration = 60.seconds, idleTimeInPool: FiniteDuration = 30.seconds): Resource[F, Client[F]] =
     EmberClientBuilder
       .default[F]
-      .withTimeout(c.timeout)
-      .withIdleTimeInPool(c.idleTimeInPool)
+      .withTimeout(timeout)
+      .withIdleTimeInPool(idleTimeInPool)
       .build
-
-
 
   implicit val logger: Logger[IO] = Slf4jLogger.getLogger[IO]
 
   def runClient = {
-    Logger[IO].info(s"Loaded config $clientC")
-    newClient[IO](clientC)
+    //Logger[IO].info(s"Loaded config $clientC")
+    newClient[IO]()
       .map { client =>
-        ClientToApp(client, HttAppTry.serverConfig.uri)
+        ClientToServer(client, UriConfig)
       }
       .use { capp =>
         repeatMonton(capp.getProds, 1, 10L) //, .postProds(ProdName("Sepp"))
@@ -278,4 +261,3 @@ object HttAppClientTry extends IOApp.Simple {
     fs2.Stream.fixedRate[IO](delay.seconds).take(nmal).evalMap(_ => ioa).compile.toList
 
 }
-
